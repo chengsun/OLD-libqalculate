fa38436237682b07a691f669cd1228d9276cf7c8
jjstickel <jjstickel>
2010-01-05 14:50:25 +0000

    bump version number in preparation for release

--- libqalculate/Calculator.h
+++ libqalculate/Calculator.h
@@ -52,7 +52,7 @@
 *
 * If the program uses autoconf, put the following in configure.in:
 * \code PKG_CHECK_MODULES(QALCULATE, [
-*	libqalculate >= 0.9.6
+*	libqalculate >= 0.9.7
 *	])
 * AC_SUBST(QALCULATE_CFLAGS)
 * AC_SUBST(QALCULATE_LIBS) \endcode
